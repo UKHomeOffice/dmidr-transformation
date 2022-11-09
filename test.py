7e476f336125ad4da49b8e5f725bfdc5fdb4be69
MichaelCullenMadeTech <michael.cullen@madetech.com>
2022-11-09 11:37:22 +0000

    Cast port as int:

--- test.py
+++ test.py
@@ -1,24 +1,27 @@
 from pymysql import connect, cursors
 import os
 
+
 def create_db_connection():
     return connect(
         host=os.environ.get("replica_db_host"),
         user=os.environ.get("replica_db_username"),
         password=os.environ.get("replica_db_password"),
         database=os.environ.get("replica_db_name"),
-        port=os.environ.get("replica_db_port"),
+        port=int(os.environ.get("replica_db_port")),
         cursorclass=cursors.DictCursor,
     )
+
 
 def get_comp_performance():
     connection = create_db_connection()
 
     with connection.cursor() as cursor:
         result = select_top_ten(cursor)
-        
+
     for row in result:
         print(row[0])
+
 
 def select_top_ten(cursor):
 
