52e7c83fc59bca310736a8a4c3b3949a3c150c70
MichaelCullenMadeTech <michael.cullen@madetech.com>
2022-11-09 12:00:11 +0000

    Changed from MySQL to Postgres

--- test.py
+++ test.py
@@ -1,16 +1,37 @@
-from pymysql import connect, cursors
+import psycopg2
 import os
 
 
 def create_db_connection():
-    return connect(
-        host=os.environ.get("replica_db_host"),
-        user=os.environ.get("replica_db_username"),
-        password=os.environ.get("replica_db_password"),
-        database=os.environ.get("replica_db_name"),
-        port=int(os.environ.get("replica_db_port")),
-        cursorclass=cursors.DictCursor,
-    )
+    conn = None
+    try:
+        params = config()
+        print('Connecting to the Audit database...')
+        conn = psycopg2.connect(
+            host=os.environ.get("replica_db_host"),
+            user=os.environ.get("replica_db_username"),
+            password=os.environ.get("replica_db_password"),
+            database=os.environ.get("replica_db_name"),
+            port=int(os.environ.get("replica_db_port")),
+            cursorclass=cursors.DictCursor,
+        )
+        cur = conn.cursor()
+
+        print('Audit database version:')
+        cur.execute('SELECT version()')
+
+        db_version = cur.fetchone()
+        print(db_version)
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+    return conn
 
 
 def get_comp_performance():
