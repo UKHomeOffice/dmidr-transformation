fe4d25f544533d82a155bb5433c867b6e3eef300
MichaelCullenMadeTech <michael.cullen@madetech.com>
2022-11-15 08:09:30 +0000

    give me top 10 to analyse the data

--- transformations/models/ukvi/cases_due_by_day.sql
+++
@@ -1,7 +0,0 @@
-select
-    "deadline",
-    count(*) as cases_due
-from {{ source('decs_raw', 'case_data') }}
-where "deadline" is not null and "deadline" != ''
-group by "deadline"
-order by "deadline"
--- transformations/models/ukvi/cleaned_case_data.sql
+++
@@ -1,9 +0,0 @@
-select
-    "timestamp",
-    "event",
-    "reference",
-    "deadline",
-    "DateReceived"
-from {{ source('decs_raw', 'case_data') }}
-where
-    "event" = 'CASE_CREATED' or "event" = 'CASE_COMPLETED'
