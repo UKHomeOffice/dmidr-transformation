870bcc90d65b5f5bdc2ca33f3a6b856ebac975fe
tf75 <kingsleytfwork@gmail.com>
2022-11-17 14:32:09 +0000

    first commit of options getting json data

---
+++ transformations/models/json_data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS payload_cols 
+AS SELECT audit_payload::json->'audit_payload'->>'type' as type_col, 
+audit_payload::json->'audit_payload'->>'uuid' as uuid_col from transformation.audit_event;
+
+
+CREATE TABLE IF NOT EXISTS payload_cols (type_col VARCHAR(500), uuid_col VARCHAR(500));
+INSERT INTO payload_cols 
+SELECT audit_payload::json->'audit_payload'->>'type' as type_col, 
+audit_payload::json->'audit_payload'->>'uuid' as uuid_col from transformation.audit_event;
