3289c130a6108854b57925a53ed508e8bf9b6e8d
tf75 <kingsleytfwork@gmail.com>
2022-11-18 11:46:27 +0000

    added in local dev work

--- transformations/models/payload/json_data.sql
+++ transformations/models/payload/json_data.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS case_table (case_type VARCHAR(500), case_deadline DATE, case_uuid VARCHAR(500));
+
 INSERT INTO case_table (case_type, case_deadline, case_uuid)
 SELECT audit_payload::json->'audit_payload'->>'type', 
 TO_DATE(audit_payload::json->'audit_payload'->>'caseDeadline', 'YYYY-MM-DD'),
