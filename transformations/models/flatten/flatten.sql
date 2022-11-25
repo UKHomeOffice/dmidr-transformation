744d73dd0d6fe5d3c8993957d79ff7a5e1bee534
MichaelCullenMadeTech <michael.cullen@madetech.com>
2022-11-25 14:53:03 +0000

    First transform done gpsh

--- transformations/models/flatten/flatten.sql
+++ transformations/models/flatten/flatten.sql
@@ -1,13 +1,13 @@
 WITH CASES AS (
     SELECT case_uuid,
-           audit_payload::json ->> 'type',
-           TO_DATE(LEFT(audit_payload::json ->> 'created', 9), 'YYYY-MM-DD'),
-           TO_DATE(audit_payload::json ->> 'dateReceived', 'YYYY-MM-DD'),
-           TO_DATE(audit_payload::json ->> 'caseDeadline', 'YYYY-MM-DD'),
-           audit_payload::json ->> 'reference',
-           audit_payload::json -> 'data' ->> 'bus_area',
-           audit_payload::json -> 'data' ->> 'allocatedToUUID'
-    FROM replica.audit_event;
+           audit_payload::json ->> 'type' AS case_type,
+           TO_DATE(LEFT(audit_payload::json ->> 'created', 9), 'YYYY-MM-DD') AS date_created,
+           TO_DATE(audit_payload::json ->> 'dateReceived', 'YYYY-MM-DD') AS date_received,
+           TO_DATE(audit_payload::json ->> 'caseDeadline', 'YYYY-MM-DD') AS case_deadline,
+           audit_payload::json ->> 'reference' AS case_reference,
+           audit_payload::json -> 'data' ->> 'bus_area' AS business_area,
+           audit_payload::json -> 'data' ->> 'allocatedToUUID' AS allocated_to_uuid
+    FROM {{source('audit_data', 'audit_event')}}
 )
 
-SELECT * FROM CASES;
+SELECT * FROM CASES
