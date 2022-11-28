6dc5360656a43c097a1f3265af14633cf11aa67e
MichaelCullenMadeTech <michael.cullen@madetech.com>
2022-11-28 08:05:01 +0000

    Change seed data

--- dbreplica/init.sql
+++ dbreplica/init.sql
@@ -22,52 +22,28 @@ CREATE TABLE IF NOT EXISTS replica.audit_event
     CONSTRAINT audit_event_uuid_idempotent UNIQUE (uuid, audit_timestamp, type)
     );
 
-INSERT INTO replica.audit_event(
-    id, 
-    uuid, 
-    case_uuid, 
-    stage_uuid, 
-    correlation_id, 
-    raising_service, 
-    audit_payload, 
-    namespace, 
-    audit_timestamp, 
-    type, 
-    user_id, 
-    case_type, 
-    deleted)
-VALUES(
-    1, 
-    uuid_generate_v1(), 
-    uuid_generate_v1(), 
-    uuid_generate_v1(), 
-    '1', 
-    'hocs-casework-794d5dbd56-5tw6s', 
+INSERT INTO replica.audit_event(id, uuid, case_uuid, stage_uuid, correlation_id, raising_service, audit_payload, namespace, audit_timestamp, type, user_id, case_type, deleted)
+VALUES(1, uuid_generate_v1(), uuid_generate_v1(), uuid_generate_v1(), '1', 'hocs-casework-79433dbd56-5tw22', 
     '{ 
         "data": 
         {
             "valid": "true", 
-            "DateReceived": "2019-09-11", 
+            "DateReceived": "2022-11-28", 
             "CopyNumberTen": "TRUE", 
             "Correspondents": "30bea4ea-3c10-46da-9c3f-cf972a7c32b5", 
             "OriginalChannel": "EMAIL", 
-            "DateOfCorrespondence": "2019-09-10"
+            "DateOfCorrespondence": "2022-11-28"
         },
-        "type": "MIN", 
-        "uuid": "e4bc767d-700c-40da-9565-017f4a24b0a1", 
-        "created": "2019-09-11T12:37:28.752136", 
-        "reference": "MIN/0120001/19", 
-        "caseDeadline": "2019-10-09", 
-        "dateReceived": "2019-09-11", 
+        "type": "MPAM", 
+        "uuid": "22bc767d-700c-40da-9565-017f4a24b0a1", 
+        "created": "2022-11-28T12:37:28.752136", 
+        "reference": "MPAM/0222222/22", 
+        "caseDeadline": "2022-11-29", 
+        "dateReceived": "2022-11-27", 
         "primaryTopic": "None", 
-        "primaryCorrespondent": "30bea4ea-3c10-46da-9c3f-cf972a7c32b5",
-        "fullname": "Adam Afriyie MP",
-        "allocatedToUUID": "1102b26b-06ed-4247-a1b3-699167f2dbcd"
+        "primaryCorrespondent": "22bea4ea-3c10-46da-9c3f-cf972a7c32b5",
+        "fullname": "Mrs Smith MP",
+        "allocatedToUUID": "2202b26b-06ed-4247-a1b3-699167f2dbcd"
         }',
-    'cs-dev', 
-    NOW(), 
-    'CASE_CREATED', 
-    '593c6e02-ece0-499a-b967-404a20dac380', 
-    'a1', 
-    FALSE
+    'cs-dev', NOW(), 'CASE_CREATED', '593c6e02-ece0-499a-b967-404a20dac380', 'a1', FALSE
 );
