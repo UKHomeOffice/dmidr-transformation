b1f9eed4afc24098ab87fe25006b77be5e6996f9
--unset <Rhys.Jones@madetech.com>
2022-12-02 09:51:04 +0000

    Change out of service standard to be case_deadline greater than now

--- transformations/models/MPAM_due_cases/mpam_due_cases_aggregate.sql
+++ transformations/models/MPAM_due_cases/mpam_due_cases_aggregate.sql
@@ -7,7 +7,7 @@ WITH mpam_due_cases_aggregate_data AS (
     FROM (
         SELECT CASE WHEN case_deadline BETWEEN date_trunc('week', NOW()::timestamp) AND date_trunc('week', NOW()::timestamp) + interval '7 day' THEN 1 ELSE 0 END as "Due this week",
                CASE WHEN case_deadline BETWEEN date_trunc('week', NOW()::timestamp) AND date_trunc('week', NOW()::timestamp) + interval '28 day' THEN 1 ELSE 0 END as "Due in next 4 weeks",
-               CASE WHEN case_deadline < NOW() THEN 1 ELSE 0 END as "Out of service standard"
+               CASE WHEN case_deadline > NOW() THEN 1 ELSE 0 END as "Out of service standard"
 
         FROM {{ ref('merged_cases') }}
         WHERE case_type = 'MPAM'
