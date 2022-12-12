816df96b83dceb8a6861f79096bc1c5be504681c
MichaelCullenMadeTech <michael.cullen@madetech.com>
2022-12-12 15:35:33 +0000

    Comp and comp2 closed cases

---
+++ transformations/models/closed_cases/comp2_aggregate_closed_cases_by_age.sql
@@ -0,0 +1,9 @@
+WITH comp2_aggregate_closed_cases_by_age AS (
+    SELECT COUNT("Case ID") as "Total cases closed",
+           "Age" as "Age (days)"
+    FROM {{ ref('closed_cases') }}
+    WHERE user_group = 'MPAM'
+    GROUP BY "Age"
+)
+
+SELECT * FROM comp2_aggregate_closed_cases_by_age
---
+++ transformations/models/closed_cases/comp_aggregate_closed_cases.sql
@@ -0,0 +1,16 @@
+WITH comp_aggregate_closed_cases AS (
+    SELECT COUNT("Case ID") as "Total cases closed",
+           SUM("Closed inside") as "Cases closed inside of service standard",
+           SUM("Closed outside") as "Cases closed outside of service standard"
+    
+    FROM (
+        SELECT "Case ID",
+               CASE WHEN "Outside Service Standard" = 1 THEN 1 ELSE 0 END as "Closed outside",
+               CASE WHEN "Outside Service Standard" = 0 THEN 1 ELSE 0 END as "Closed inside"
+
+        FROM {{ ref('closed_cases') }}
+        WHERE user_group = 'MPAM'
+    ) as closed_cases
+)
+
+SELECT * FROM comp_aggregate_closed_cases
