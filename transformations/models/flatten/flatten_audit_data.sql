6b66295bce1e4e50b6c624740c660f4bce7ff0b6
--unset <Rhys.Jones@madetech.com>
2022-12-15 15:19:03 +0000

    Preventing inserting data. Changing business area field

--- transformations/models/flatten/flatten_audit_data.sql
+++ transformations/models/flatten/flatten_audit_data.sql
@@ -12,7 +12,7 @@ WITH cases AS (
            TO_DATE(audit_payload::json ->> 'dateReceived', 'YYYY-MM-DD') AS date_received,
            TO_DATE(audit_payload::json ->> 'caseDeadline', 'YYYY-MM-DD') AS case_deadline,
            audit_payload::json ->> 'reference' AS case_reference,
-           audit_payload::json -> 'data' ->> 'bus_area' AS business_area,
+           audit_payload::json -> 'data' ->> 'BusArea' AS business_area,
            audit_payload::json -> 'data' ->> 'allocatedToUUID' AS allocated_to_uuid,
            audit_timestamp AS audit_timestamp,
            audit_payload::json ->> 'stage' AS stage
