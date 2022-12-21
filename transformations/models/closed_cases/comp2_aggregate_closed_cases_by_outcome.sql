18b1e04b51a2044003f897c14780d84abaa01db6
MichaelCullenMadeTech <michael.cullen@madetech.com>
2022-12-21 08:23:34 +0000

    Closed cases
    Add the correct models and fields
    Still don't know how to calculate upheld, partially upheld, and not upheld.
    Rest are quite simple reuasble tests

--- transformations/models/closed_cases/comp2_aggregate_closed_cases_by_outcome.sql
+++ transformations/models/closed_cases/comp2_aggregate_closed_cases_by_outcome.sql
@@ -1,5 +1,7 @@
 WITH comp2_aggregate_closed_cases_by_outcome AS (
-    SELECT COUNT("Case ID") as "Total cases closed"
+    SELECT COUNT("Case ID") as "Upheld",
+    COUNT("Case ID") as "Partially upheld",
+    COUNT("Case ID") as "Not upheld"
 
     FROM {{ ref('closed_cases') }}
     WHERE user_group = 'COMP2'
