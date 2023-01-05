9f433bf4d353afd8dd5c669894c15f9758cce2d7
MichaelCullenMadeTech <michael.cullen@madetech.com>
2023-01-05 09:18:42 +0000

    Add business area to closed cases by outcome report

--- transformations/models/closed_cases/comp2_aggregate_closed_cases_by_outcome.sql
+++ transformations/models/closed_cases/comp2_aggregate_closed_cases_by_outcome.sql
@@ -1,10 +1,13 @@
 WITH comp2_aggregate_closed_cases_by_outcome AS (
     SELECT COUNT("Case ID") as "Upheld",
     COUNT("Case ID") as "Partially upheld",
-    COUNT("Case ID") as "Not upheld"
+    COUNT("Case ID") as "Not upheld",
+    "Business Area"
 
     FROM {{ ref('closed_cases') }}
     WHERE user_group = 'COMP2'
+
+    GROUP BY "Business Area"
 
 )
 
--- transformations/models/closed_cases/mpam_aggregate_closed_cases_by_outcome.sql
+++ transformations/models/closed_cases/mpam_aggregate_closed_cases_by_outcome.sql
@@ -1,10 +1,13 @@
 WITH mpam_aggregate_closed_cases_by_outcome AS (
     SELECT COUNT("Case ID") as "Upheld",
     COUNT("Case ID") as "Partially upheld",
-    COUNT("Case ID") as "Not upheld"
+    COUNT("Case ID") as "Not upheld",
+    "Business Area"
 
     FROM {{ ref('closed_cases') }}
     WHERE user_group = 'MPAM'
+
+    GROUP BY "Business Area"
 
 )
 
